c2a612cc44be0b50b8bdc1c9a65f8927881daeda
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2025-02-10 15:20:27 +0400

    create json file imformation about photos and sizes

--- API_Vk.py
+++ API_Vk.py
@@ -33,18 +33,19 @@ class VkAPI:
         response = requests.get(url, params=params).json()
         image = response.get("response", {}).get("items", [])
 
-        image_info = []
+        photo_info = []
         for img in image:
             max_size_photo = max(img["sizes"], key=lambda x: x["type"] == "z")
             photo_url = max_size_photo["url"]
             file_name_likes = img["likes"]["count"]
 
-            image_info.append({
+            photo_info.append({
                 "file_name": file_name_likes,
-                "size": max_size_photo["type"]
+                "size": max_size_photo["type"],
+                "url": photo_url
             })
             
-        return image_info
+        return photo_info
 
 
 class JdAPI:
@@ -61,10 +62,10 @@ class JdAPI:
         return response.status_code
 
     """Загрузка фото на Яндекс Диск с использованием модуля прогресс-бар"""
-    def upload_photos(self, file_name_likes):
+    def upload_photos(self, photo_url, file_name_likes):
         url = f"{self.base_url}resources/upload"
         file_name = f"{file_name_likes}_likes.jpg"
-        params = {"path": f"VK_photos/{file_name}"}
+        params = {"path": f"VK_photos/{file_name}", "url": photo_url}
 
         with tqdm(desc=f"Загрузка {file_name}", unit="B", unit_scale=True, unit_divisor=1024) as progress_bar:
             response = requests.post(url, params=params, headers=self.headers, stream=True)
@@ -76,13 +77,23 @@ class JdAPI:
             else:
                 return f"Ошибка {response.status_code}: {response.text}"
 
+"""Создание json файла с инфо о фото"""
+def photos_json(vk_api_photos):
+    with open("vk_api_photos.json", "w") as file:
+        json.dump(vk_api_photos, file, ensure_ascii=False, indent=4)
+
 
 vk_api = VkAPI(vk_token)
 jd_api = JdAPI(jd_token)
 
-user_id = int(input("Введите ID пользователя: "))
-vk_api.get_photos(user_id)
-
+user_id = int(input("Введите ID пользователя: ")) # 69377195
+vk_api_photos = vk_api.get_photos(user_id)
 jd_api.create_folder()
-jd_api.upload_photos()
 
+for photo_info in vk_api_photos:
+    file_name = photo_info["file_name"]
+    photo_url = photo_info["url"]
+    print(jd_api.upload_photos(photo_url, file_name))
+
+
+photos_json(vk_api_photos)
